7d4b359a8ce01055e625064b94a1517195702260
Fuk Hak <fukhak@rbq.pw>
2021-01-22 18:52:50 +0800

    重寫文件驗證

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+export BGMI_LOG=debug
 if [ $1 = "start" ]; then
-    if [ -z "$(ls -A $BGMI_PATH)" ]; then
-        echo "Can't find any file in /app folder. initialing"
+    if [ ! -f /app/initfile ]; then
+        echo "Can't find initfile in /app folder. initialing"
+        touch /app/initfile
         bgmi
         echo "Get environment and configure"
         /script/config.sh
@@ -12,6 +14,7 @@ if [ $1 = "start" ]; then
         fi
     fi
     bgmi install
+    bgmi cal --download-cover
     bgmi_http --port=80 --address=0.0.0.0
 else
     $@
